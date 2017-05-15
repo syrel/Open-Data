5732f4db1c09c4348a6fe9d8208541b2ba0bb4b7
Aliaksei Syrel <alex.syrel@gmail.com>
2017-05-15 22:35:11 +0200

    Cleaned up index.jsx

--- app/src/client/app/index.jsx
+++ app/src/client/app/index.jsx
@@ -12,9 +12,6 @@ import { FormControl } from 'react-bootstrap';
 
 import Inspector from './framework/Inspector';
 
-import YASQE from 'yasgui-yasqe';
-import Sparql from './Sparql';
-
 import LEndpoint from './model/LEndpoint';
 
 class App extends React.Component {
@@ -22,50 +19,17 @@ class App extends React.Component {
         super(props);
 
         this.state = {
-            yasqe: null,
-            endpoint: 'https://ld.geo.admin.ch/query',
+            endpoint: new LEndpoint('https://ld.geo.admin.ch/query'),
         };
 
-        // this.results = new TablePresentation()
-        //     .header(xml => xml.root.children[0].children.map(each => each.attributes.name))
-        //     .dynamic((header, column) => {
-        //         column
-        //             .named(() => header)
-        //             .evaluated(result => result[column.index()].content + ' ('+(result[column.index()].name) + ')')
-        //     })
-        //     .display(xml => xml.root.children[1].children)
-        //     .transformed(result => result.children.map(each => { return {
-        //         datatype: each.children[0].attributes.datatype,
-        //         name: each.children[0].name,
-        //         content:  each.children[0].content
-        //     }}));
-
-        //var endpoint = new LEndpoint('http://lindas-data.ch/sparql');
-        var endpoint = new LEndpoint('https://ld.geo.admin.ch/query');
         this.inspector = new Inspector();
-        this.inspector.openOn(endpoint);
-    }
-
-    onClick (callback) {
-        callback();
-
-        // const source = Sparql.query(ReactDOM.findDOMNode(this.refs.endpoint).value, this.state.yasqe.getValue());
-        // source.then(result => this.results.on(result), (err) => {
-        //     console.error(err);
-        //     callback()
-        // });
-    }
-
-    componentDidMount() {
-       // this.state.yasqe = YASQE.fromTextArea(document.getElementById('query'));
-    }
-
-    componentDidUpdate() {
-        //this.state.yasqe = YASQE.fromTextArea(document.getElementById('query'));
+        this.inspector.openOn(this.state.endpoint);
     }
 
     handleChange() {
-        this.state.endpoint = ReactDOM.findDOMNode(this.refs.endpoint).value;
+        this.setState({
+            endpoint: new LEndpoint(ReactDOM.findDOMNode(this.refs.endpoint).value)
+        });
     }
 
     render () {
@@ -75,23 +39,11 @@ class App extends React.Component {
                     <FormGroup>
                         <InputGroup>
                             <InputGroup.Addon>Endpoint:</InputGroup.Addon>
-                            <FormControl type="text" ref='endpoint' value={this.state.endpoint} onChange={this.handleChange}/>
+                            <FormControl type="text" ref='endpoint' value={this.state.endpoint.getUri()} onChange={this.handleChange}/>
                         </InputGroup>
                     </FormGroup>
                 </form>
-                {/*<textarea id="query">{`select distinct ?solutionName ?providerName ?providerUID ?hasSolutionProvider*/}
-{/*where {*/}
-	{/*?s a <http://lindas-data.ch/def/top/SoftwareSolution> ; <http://lindas-data.ch/def/top/hasSolutionProvider> ?sp .*/}
-	{/*?sp <http://lindas-data.ch/def/top/uid> ?providerUID .*/}
-	{/*?s <http://www.w3.org/2004/02/skos/core#prefLabel> ?solutionName.*/}
-	{/*?sp <http://www.w3.org/2004/02/skos/core#prefLabel> ?providerName.*/}
-{/*}*/}
-{/*order by ?sp*/}
-{/*`}</textarea>*/}
-
-                {/*{ this.results.render() }*/}
                 { this.inspector.render() }
-
             </div>
         )
     }
